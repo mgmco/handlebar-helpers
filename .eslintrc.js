0d3e24fa77c8122e5fde7e5728cc680c4081ea32
mtimofiiv <github@fiiv.io>
2018-02-15 09:53:50 +0100

    adding eslint + making sure files pass

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": ["airbnb-base"],
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module"
+  },
+  "rules": {
+    "global-require": ["off"],
+    "comma-dangle": ["error", "never"],
+    "no-plusplus": ["off"],
+    "arrow-parens": ["off"],
+    "no-restricted-syntax": ["off"],
+    "no-console": ["off"],
+    "no-continue": ["off"],
+    "import/no-unresolved": [2, {}],
+    "import/prefer-default-export": ["off"],
+    "class-methods-use-this": ["off"]
+  }
+}
